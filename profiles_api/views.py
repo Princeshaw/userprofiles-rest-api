a50d3673196cf30fd2bc579b025f26ce6131fcf8
Princeshaw <prince54shaw@gmail.com>
2020-08-14 21:34:15 +0530

    added viewset to profile API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from profiles_api import serializers
 
@@ -33,3 +34,37 @@ class HelloApiView(APIView):
         return Response({'method':'PATCH'})
     def delete(self,request,pk=None):
         return  Response({'method':'DELETE'})
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+    serializer_class = serializers.HelloSerializers
+    def list(self,request):
+        """Return a hello message"""
+        a_viewset = ['Uses HTTP methods as function(get,post,patch,put,delete)','Is similar to traditional Django View',
+                      'gives you the most control over you application logic','Is mapped manually to urls',]
+        return  Response({'message':'Hello!','ViewSet':a_viewset})
+    def create(self,request):
+        """Create new hello message"""
+        serializer = self.serializer_class(data = request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+            )
+    def retrieve(self,request,pk=None):
+        """Handled getting an object by its ID"""
+        return Response({'http_method','GET'})
+    def update(self,request,pk=None):
+        """Handle updating an object"""
+        return Response({'http_method','PUT'})
+    def partial_update(self,request,pk=None):
+        return Response({'http_method','PATCH'})
+    def destroy(self,request,pk=None):
+        """handle removing an object"""
+        return Response({'http_method':'DELETE'})
+
+
